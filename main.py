d43615200005a3acf7834c51638ca0bd3ebf556b
Jack Luo <jack145945@gmail.com>
2024-05-09 11:25:27 -0400

    v0.1.1: init boilerplate

---
+++ main.py
@@ -0,0 +1,38 @@
+from langchain.llms import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from langchain import PromptTemplate, LLMChain
+from langchain import HuggingFaceHub
+
+model_id = "gpt2"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id)
+pipe = pipeline(
+    "text-generation", model=model, tokenizer=tokenizer, max_new_tokens=64
+)
+hf = HuggingFacePipeline(pipeline=pipe)
+
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+prompt = PromptTemplate(template=template, input_variables=["question"])
+llm_chain = LLMChain(prompt=prompt, llm=hf)
+
+question = "Who won the FIFA World Cup in the year 1994? "
+
+print(llm_chain.run(question))
+
+
+repo_id = "google/flan-t5-xl" # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
+
+llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature":0, "max_length":64})
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+prompt = PromptTemplate(template=template, input_variables=["question"])
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+question = "Who won the FIFA World Cup in the year 1994? "
+
+print(llm_chain.run(question))
