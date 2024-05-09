e8efc00d465430898a1ab6a000fa1d7d4f4f9f87
Jack Luo <jack145945@gmail.com>
2024-05-09 12:15:27 -0400

    v0.1.1: test.py wrote a test program for python pipeline testing
    Co-authored-by: kevz2026 <kevz2026@users.noreply.github.com>

---
+++ test.py
@@ -0,0 +1,7 @@
+"""
+name: test.py
+description: a test file to ensure that python is working and a sandbox for testing code
+"""
+
+print("Hello")
+print("This is live while we code") # prints a message
