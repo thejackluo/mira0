cb5b593851d55950196b07942f317c7ca2c540e1
Jack Luo <jack145945@gmail.com>
2024-05-15 21:54:41 -0400

    v0.1.1: removed test.py for redundancy
    Co-authored-by: kevz2026 <kevz2026@users.noreply.github.com>

--- test.py
+++
@@ -1,7 +0,0 @@
-"""
-name: test.py
-description: a test file to ensure that python is working and a sandbox for testing code
-"""
-
-print("Hello")
-print("This is live while we code") # prints a message
